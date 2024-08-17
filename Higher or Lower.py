8ad57733193f66a4e0cc4ce96272fb03d301e9ce
Carlosgit config --global user.email <Carlos>
2024-08-17 19:03:54 -0400

    fixed 'out of range' error in getting_profile added greater following function.

--- Higher or Lower.py
+++ Higher or Lower.py
@@ -5,12 +5,26 @@ from random import randint
 print(art.logo, art.vs)
 profile_dict = game_data.data
 def getting_profile():
-    profile_A = profile_dict[randint(0, len(profile_dict))]
-    profile_B = profile_dict[randint(0, len(profile_dict))]
+    profile_A = profile_dict[randint(0, len(profile_dict)-1)]
+    profile_B = profile_dict[randint(0, len(profile_dict)-1)]
     if profile_A == profile_B:
-        getting_profile()
-    print(profile_A,"\n",profile_B)
-getting_profile()
+        getting_profile()  
+    return profile_A, profile_B
+
+def greater_following(prof_A, prof_B):
+    for key in prof_A and prof_B:
+        if key == 'follower_count':
+            sum1 = prof_A[key]
+            sum2 = prof_B[key]
+        if sum1 > sum2:
+            return sum1
+        else:
+            return sum2   
+
+#main program
+profile_A, profile_B = getting_profile()
+higher_followinf = greater_following(profile_A, profile_B)
+
 #Process options for who has bigger following
 
 #either keep track of score or end game
