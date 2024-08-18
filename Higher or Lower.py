74a1d2bd30ee2bf315d664617469427f4cfb04d5
Carlosgit config --global user.email <Carlos>
2024-08-17 22:04:03 -0400

    added user_response_process function and edited greater_following function

--- Higher or Lower.py
+++ Higher or Lower.py
@@ -4,6 +4,7 @@ from random import randint
 # ascii art and show user two options
 print(art.logo, art.vs)
 profile_dict = game_data.data
+
 def getting_profile():
     profile_A = profile_dict[randint(0, len(profile_dict)-1)]
     profile_B = profile_dict[randint(0, len(profile_dict)-1)]
@@ -12,30 +13,37 @@ def getting_profile():
     return profile_A, profile_B
 
 def greater_following(prof_A, prof_B):
+    """Function is to figure out who has higher following and to return the correct answer the program is looking for."""
+    sum1 = []
+    sum2 = []
     for key in prof_A and prof_B:
         if key == 'follower_count':
-            sum1 = prof_A[key]
-            sum2 = prof_B[key]
-        if sum1 > sum2:
-            return sum1
-        else:
-            return sum2   
+            sum1.append(prof_A[key])
+            sum2.append(prof_B[key])
+            print(sum1, sum2)           
+            if sum1 > sum2:
+                sum1.append('A')
+                return sum1
+            else:
+                sum2.append('B')
+                return sum2   
+
+def user_response_process(high_foll):
+    answer = input("Type 'A', if A has higher following or type 'B', if B has a higher following:").upper()
+    if answer == high_foll[1]:
+        print("correct")
+        #loop to next profiles
+    else:
+        return'wrong'
 
 #main program
 profile_A, profile_B = getting_profile()
-higher_followinf = greater_following(profile_A, profile_B)
-
-#Process options for who has bigger following
-
+higher_following = greater_following(profile_A, profile_B)
+user_response_process(higher_following)
 #either keep track of score or end game
 
 #Randomly iterarte through dictionary for next comparison
 
-# add fault tolerance for same comparison
-
-#Ascii art repeatedly show up for all comparisons. 
-
-
-
+#Ascii art repeatedly show up for all comparisons.
 
 
